d3830e19259ece9f6741343a1b05ace8a9c4c318
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-18 20:05:11 -0300

    Update iterator.rs

--- src/iterator.rs
+++ src/iterator.rs
@@ -8,7 +8,8 @@ struct ThreadIter {
     index: usize,
     bytecode: &ByteCode,
     scope: HashMap<&str, isize>,
-    stack: Vec<isize>
+    stack: Vec<isize>,
+    block: Vec<usize>
 }
 
 impl ThreadIter {
@@ -22,16 +23,9 @@ impl ThreadIter {
             index: index,
             bytecode: bytecode,
             scope: HashMap::new(),
-            stack: vec![]
+            stack: vec![],
+            block: vec![]
         }
-    }
-
-    fn read(&self) -> Option<Instruction> {
-        let line = self.bytecode.get(self.index);
-        if let Some(_)=line {} else {
-            self.active = false;
-        };
-        line
     }
 
     pub fn goto(&self, index: usize) -> Self {
@@ -42,5 +36,26 @@ impl ThreadIter {
     pub fn next(&self) -> Self {
         self.index = self.index + 1;
         self
+    }
+
+    fn read(&self) -> Option<Instruction> {
+        let line = self.bytecode.get(self.index);
+        if let Some(_)=line {} else {
+            self.active = false;
+        };
+        line
+    }
+
+    pub fn eval() -> Option<isize> {
+        if let Some(instruct)=self.read() {
+            match Code::eval(
+                &mut self,
+                instruct.0,
+                instruct.1
+            ) {
+                Ok(value) => value,
+                Err(error) => {self.alive = false},
+            }
+        }
     }
 }
