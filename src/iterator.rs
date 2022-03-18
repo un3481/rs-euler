54b1bac78d95098f7a11c7f8d17205cd36ba268d
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-18 18:01:26 -0300

    Update iterator.rs

--- src/iterator.rs
+++ src/iterator.rs
@@ -1,1 +1,40 @@
 
+use crate::bytecode::{
+    ByteCode
+};
+
+struct ThreadIter {
+    alive: bool,
+    location: usize,
+    bytecode: &ByteCode,
+    scope: HashMap<&str, isize>,
+    stack: Vec<isize>
+}
+
+mod ThreadIter {
+
+    pub fn new(
+        code: &ByteCode,
+        index: usize
+    ) -> ThreadIter {
+        ThreadIter {
+            alive: true,
+            location: index,
+            scope: HashMap::new(),
+            stack: vec![]
+        }
+    }
+}
+
+impl ThreadIter {
+
+    fn goto(self: &Self, index: usize) -> &Self {
+        *self.location = index;
+        self
+    }
+
+    fn next(self: &Self) -> Some(Instruction) {
+        *self.location = *self.location + 1;
+        self.bytecode[self.location]
+    }
+}
