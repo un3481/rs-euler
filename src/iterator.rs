ce74e62122e4a5049fa103ae765042b96c8895cc
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-03-20 19:33:52 -0300

    Update iterator.rs

--- src/iterator.rs
+++ src/iterator.rs
@@ -4,9 +4,9 @@ use crate::bytecode::{
     Instruction
 };
 
-type ParentScope = Box<(usize, ScopeIter)>;
+type ParentScope = Box<(usize, ThreadScope)>;
 
-struct ScopeIter {
+struct ThreadScope {
     parent: Option<ParentScope>,
     names: [isize; 256],
     block: [u8; 128],
@@ -14,30 +14,30 @@ struct ScopeIter {
     flags: [u8; 8],
     blen: u8,
     slen: u8
-}
+};
 
-struct ThreadIter {
+type ThreadError = (usize, &str);
+
+struct GreenThread {
     alive: bool,
     index: usize,
     bytecode: &ByteCode,
-    scope: ScopeIter,
-    error: Option<(usize, &str)>
-}
+    scope: ThreadScope,
+    error: Option<ThreadError>
+};
 
-impl ThreadIter {
+impl GreenThread {
 
     #[inline(always)]
     pub fn new(
         bytecode: &ByteCode,
         index: usize
     ) -> Self {
-        ThreadIter {
+        GreenThread {
             alive: true,
             index: index,
             bytecode: bytecode,
-            scope: HashMap::new(),
-            stack: vec![],
-            block: vec![],
+            scope: ThreadScope::new(),
             error: None
         }
     }
@@ -50,61 +50,57 @@ impl ThreadIter {
 
     #[inline(always)] 
     pub fn next(&mut self) -> Self {
-        self.goto(self.index + 1);
-        self
+        self.goto(self.index + 1)
     }
 
     #[inline(always)]
-    fn read(&self) -> Option<(u8, Vec<isize>)> {
+    fn read(&self) -> Option<Instruction> {
         let line = self.bytecode.get(self.index);
-        if let Some(_)=line {} else {
-            self.active = false;
-        };
+        if let None=line {self.active = false};
         line
     }
 
     #[inline(always)]
-    pub fn eval(&mut self) -> () {
-        if let Some(instruction)=self.read() {
-            if self.alive {
-                match self.execute(op) {
-                    (false, error) => {
-                        self.error = Some(error);
-                        self.alive = false;
-                    },
-                    _ => (),
-                }
-            }
+    pub fn eval(&mut self) -> Evaluation {
+        if !self.alive {return};
+        if let Some(instr)=self.read() {
+            Eval::instruction(self, instr)
         }
     }
+};
+
+type Evaluation = (bool, Option<String>);
+
+struct Eval {};
+impl Eval {
 
     #[inline(always)]
-    fn execute(
-        &mut self,
-        instruction: (u8, isize)
-    ) -> Result<isize, Error> {
-        let (command, args) = op;
+    fn instruction(
+        thread: &mut EulerThread,
+        instruction: Instruction
+    ) -> Evaluation {
+        let (bytecode, arg) = op;
         match command {
-            0 => Stack::push(self, args),
-            1 => Stack::pop(self),
-            2 => Scope::set(self, args),
-            3 => Scope::get(self, args),
-            31 => Math::add(self),
-            32 => Math::sub(self),
-            33 => Math::mul(self),
-            34 => Math::div(self),
-            41 => If::start(self),
-            42 => If::end(self),
-            43 => If::else(self),
-            51 => Loop::start(self),
-            52 => Loop::end(self),
-            53 => Loop::break(self),
-            54 => Loop::continue(self),
-            91 => Fun::declare(self, args),
-            92 => Fun::end(self),
-            93 => Fun::return(self),
-            94 => Fun::call(self, args),
-            _ => Error::exit(self, "invalid instruction"),
+            0 => Stack::push(thread, arg),
+            1 => Stack::pop(thread),
+            2 => Scope::set(thread, arg),
+            3 => Scope::get(thread, arg),
+            31 => Math::add(thread),
+            32 => Math::sub(thread),
+            33 => Math::mul(thread),
+            34 => Math::div(thread),
+            41 => If::start(thread),
+            42 => If::end(thread),
+            43 => If::else(thread),
+            51 => Loop::start(thread),
+            52 => Loop::end(thread),
+            53 => Loop::break(thread),
+            54 => Loop::continue(thread),
+            91 => Fun::declare(thread, arg),
+            92 => Fun::end(thread),
+            93 => Fun::return(thread),
+            94 => Fun::call(thread, arg),
+            _ => Eval::exit(thread, "invalid instruction"),
         }
     }
-}
+};
